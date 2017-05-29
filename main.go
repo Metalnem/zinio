d522e72a6aa88722d684047d63354ad8d844f5d7
Nemanja Mijailovic <metalnem@mijailovic.net>
2017-05-29 16:59:42 +0200

    URL and password retrieval working

--- main.go
+++ main.go
@@ -15,12 +15,13 @@ import (
 	"os"
 	"time"
 
+	"github.com/cenkalti/backoff"
 	"golang.org/x/net/context/ctxhttp"
 )
 
 const (
 	baseURL     = "https://services.zinio.com/newsstandServices/"
-	httpTimeout = 10 * time.Second
+	httpTimeout = 5 * time.Second
 
 	deviceID         = "A6B50079-BE65-44D6-A961-8A184AA81077"
 	installationUUID = "84A8E36D-DF7F-4D7E-9824-F793AFB93207"
@@ -29,6 +30,28 @@ const (
 var (
 	decryptionKey = []byte("8D}[" + deviceID[0:4] + "i)|z" + installationUUID[0:4])
 )
+
+// Session contains session data for single authenticated user.
+type Session struct {
+	login     string
+	password  string
+	profileID string
+}
+
+// Magazine represents single magazine with all its issues.
+type Magazine struct {
+	ID     string
+	Title  string
+	Issues []string
+}
+
+// Issue represents single magazine issue.
+type Issue struct {
+	Title     string
+	URL       string
+	Password  string
+	PageCount int
+}
 
 type version struct {
 	Key   string `xml:"key,attr"`
@@ -48,10 +71,11 @@ type zinioServiceRequest struct {
 			Password string `xml:"password"`
 		} `xml:"authorization"`
 		Device struct {
-			ProfileID        string `xml:"profileId,omitempty"`
-			DeviceID         string `xml:"deviceId"`
-			DeviceName       string `xml:"deviceName"`
-			InstallationUUID string `xml:"installationUUID"`
+			ProfileID           string `xml:"profileId,omitempty"`
+			DeviceID            string `xml:"deviceId"`
+			DeviceName          string `xml:"deviceName"`
+			InstallationUUID    string `xml:"installationUUID"`
+			PlatformDescription string `xml:"platformDescription"`
 		} `xml:"device"`
 		Application struct {
 			ApplicationName    string    `xml:"applicationName"`
@@ -66,8 +90,23 @@ type authenticateUserResponse struct {
 	ProfileID string `xml:"profileId"`
 }
 
+type library struct {
+	LibraryPublications []struct {
+		Publication struct {
+			PubID       string `xml:"pubId"`
+			DisplayName string `xml:"displayName"`
+		} `xml:"publication"`
+		LibraryIssue struct {
+			Issue struct {
+				IssueID string `xml:"issueId"`
+				Title   string `xml:"title"`
+			} `xml:"issue"`
+		} `xml:"libraryIssue"`
+	} `xml:"libraryPublication"`
+}
+
 type issuePackingList struct {
-	SingleIssue []struct {
+	SingleIssue struct {
 		PubID         string `xml:"pubId"`
 		IssueTitle    string `xml:"issueTitle"`
 		IssueID       string `xml:"issueId"`
@@ -77,7 +116,7 @@ type issuePackingList struct {
 			Init  string `xml:"init,attr"`
 			Value string `xml:",chardata"`
 		} `xml:"trackingCode"`
-		NumberOfPages string `xml:"numberOfPages"`
+		NumberOfPages int `xml:"numberOfPages"`
 	} `xml:"singleIssue"`
 }
 
@@ -91,6 +130,7 @@ type zinioServiceResponse struct {
 		} `xml:"errorDetail"`
 	} `xml:"responseStatus"`
 	AuthenticateUserResponse authenticateUserResponse `xml:"authenticateUserResponse"`
+	Library                  library                  `xml:"library"`
 	IssuePackingList         issuePackingList         `xml:"issuePackingList"`
 }
 
@@ -113,6 +153,7 @@ func makeRequest(p parameters) zinioServiceRequest {
 	h.Device.DeviceID = deviceID
 	h.Device.DeviceName = "Windows 8 Device"
 	h.Device.InstallationUUID = installationUUID
+	h.Device.PlatformDescription = "Windows 8"
 
 	h.Application.ApplicationName = "ZinioWin8"
 	h.Application.Versions = []version{{"security", "1.0"}}
@@ -125,6 +166,24 @@ func makeRequest(p parameters) zinioServiceRequest {
 	}
 
 	return req
+}
+
+func retry(ctx context.Context, query string, p parameters) (*zinioServiceResponse, error) {
+	var resp *zinioServiceResponse
+	var err error
+
+	op := func() error {
+		resp, err = post(ctx, query, p)
+		return err
+	}
+
+	b := backoff.WithContext(backoff.NewConstantBackOff(time.Second), ctx)
+
+	if err := backoff.Retry(op, b); err != nil {
+		return nil, err
+	}
+
+	return resp, nil
 }
 
 func post(ctx context.Context, query string, p parameters) (*zinioServiceResponse, error) {
@@ -167,39 +226,6 @@ func post(ctx context.Context, query string, p parameters) (*zinioServiceRespons
 	return &res, nil
 }
 
-func authenticateUser(ctx context.Context, login, password string) (*authenticateUserResponse, error) {
-	p := parameters{
-		login:    login,
-		password: password,
-	}
-
-	resp, err := post(ctx, "authenticateUser", p)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return &resp.AuthenticateUserResponse, nil
-}
-
-func issueData(ctx context.Context, login, password, profileID, pubID, issueID string) (*issuePackingList, error) {
-	p := parameters{
-		login:     login,
-		password:  password,
-		profileID: profileID,
-		pubID:     pubID,
-		issueID:   issueID,
-	}
-
-	resp, err := post(ctx, "issueData", p)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return &resp.IssuePackingList, nil
-}
-
 func decryptPdfPassword(iv64, ciphertext64 string) (string, error) {
 	iv, err := base64.StdEncoding.DecodeString(iv64)
 
@@ -225,30 +251,126 @@ func decryptPdfPassword(iv64, ciphertext64 string) (string, error) {
 	return string(ciphertext[:32]), nil
 }
 
+// Login connects to Zinio API server and authenticates user using given email and password.
+func Login(ctx context.Context, email, password string) (*Session, error) {
+	p := parameters{
+		login:    email,
+		password: password,
+	}
+
+	resp, err := retry(ctx, "authenticateUser", p)
+
+	if err != nil {
+		return nil, err
+	}
+
+	session := Session{
+		login:     email,
+		password:  password,
+		profileID: resp.AuthenticateUserResponse.ProfileID,
+	}
+
+	return &session, nil
+}
+
+// GetMagazines downloads list of all available magazines and issues.
+func (session *Session) GetMagazines(ctx context.Context) ([]Magazine, error) {
+	p := parameters{
+		login:     session.login,
+		password:  session.password,
+		profileID: session.profileID,
+	}
+
+	resp, err := retry(ctx, "libraryService", p)
+
+	if err != nil {
+		return nil, err
+	}
+
+	magazines := make(map[string]*Magazine)
+
+	for _, publication := range resp.Library.LibraryPublications {
+		pubID := publication.Publication.PubID
+		mag, ok := magazines[pubID]
+
+		if !ok {
+			mag = &Magazine{ID: pubID, Title: publication.Publication.DisplayName}
+			magazines[pubID] = mag
+		}
+
+		mag.Issues = append(mag.Issues, publication.LibraryIssue.Issue.IssueID)
+	}
+
+	var res []Magazine
+
+	for _, mag := range magazines {
+		res = append(res, *mag)
+	}
+
+	return res, nil
+}
+
+// GetIssue downloads metadata for single magazine issue.
+func (session *Session) GetIssue(ctx context.Context, magazineID, issueID string) (*Issue, error) {
+	p := parameters{
+		login:     session.login,
+		password:  session.password,
+		profileID: session.profileID,
+		pubID:     magazineID,
+		issueID:   issueID,
+	}
+
+	resp, err := post(ctx, "issueData", p)
+
+	if err != nil {
+		return nil, err
+	}
+
+	issue := resp.IssuePackingList.SingleIssue
+	password, err := decryptPdfPassword(issue.TrackingCode.Init, issue.TrackingCode.Value)
+
+	if err != nil {
+		return nil, err
+	}
+
+	isssue := Issue{
+		Title:     issue.IssueTitle,
+		URL:       fmt.Sprintf("https://%s%s", issue.HostName, issue.IssueAssetDir),
+		Password:  password,
+		PageCount: issue.NumberOfPages,
+	}
+
+	return &isssue, nil
+}
+
 func main() {
-	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cancel()
 
 	login := os.Getenv("ZINIO_EMAIL")
 	password := os.Getenv("ZINIO_PASSWORD")
 
-	auth, err := authenticateUser(ctx, login, password)
+	session, err := Login(ctx, login, password)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	issue, err := issueData(ctx, login, password, auth.ProfileID, "373124878", "416413259")
+	magazines, err := session.GetMagazines(ctx)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	code := issue.SingleIssue[0].TrackingCode
-	pdfPassword, err := decryptPdfPassword(code.Init, code.Value)
+	for _, magazine := range magazines {
+		for _, issueID := range magazine.Issues {
+			issue, err := session.GetIssue(ctx, magazine.ID, issueID)
 
-	if err != nil {
-		log.Fatal(err)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Printf("%+v\n", issue)
+		}
 	}
-
-	fmt.Println(pdfPassword)
 }
